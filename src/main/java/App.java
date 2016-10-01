f8aed5e5abc7432a48be94ab3c1b912ac15d78ba
Ben Vissotzky <ben.vissotzky@gmail.com>
2016-09-30 19:39:35 -0700

    form submission instantiates and saves to database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,10 +11,49 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-  get("/", (request, response) -> {
-    Map<String, Object> model = new HashMap<String, Object>();
-    model.put("template", "templates/index.vtl");
-    return new ModelAndView(model, layout);
-  }, new VelocityTemplateEngine());
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/sightings/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/new-sighting.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/sightings/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String ranger = request.queryParams("ranger");
+      String rangerName = request.queryParams("new-ranger-input");
+      if (ranger.equals("new-ranger") && !rangerName.equals("")) {
+        Ranger newRanger = new Ranger(rangerName);
+        System.out.println("woo");
+        newRanger.save();
+      }
+
+      String species = request.queryParams("species");
+      String speciesName = request.queryParams("new-species-input");
+      if (species.equals("new-species") && !speciesName.equals("")) {
+        Animal newAnimal = new Animal(speciesName);
+        System.out.println("woo");
+        newAnimal.save();
+      }
+
+      String location = request.queryParams("location");
+      String locationName = request.queryParams("new-location-input");
+      if (location.equals("new-location") && !locationName.equals("")) {
+        Location newLocation = new Location(locationName);
+        System.out.println("woo");
+        newLocation.save();
+      }
+
+      response.redirect("/");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+
   }
 }
